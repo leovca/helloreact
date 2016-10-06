60489fcc4b9ed804557baf85ffa52c1623014555
Leonan Teixeira <leonan.teixeira@gmail.com>
2016-10-05 23:57:11 -0300

    Add comment component

--- component/tutorial1.js
+++ component/tutorial1.js
@@ -10,4 +10,36 @@ var CommentBox = React.createClass({
   }
 });
 
+var CommentList = React.createClass({
+  render: function() {
+      return (
+        <div className='commentList'>
+          <Comment author='Leonan Teixeira'>This is one comment</Comment>
+          <Comment author='João da Silva'>This is *another* comment</Comment>
+        </div>
+      );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className='commentForm'>
+        Hello, Word! I am a CommentForm.
+      </div>
+    );
+  }
+})
+
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <div className='comment'>
+        <h2 className='commentAuthor'>{this.props.author}</h2>
+      {this.props.children}
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(<CommentBox/>, document.getElementById('content'))
