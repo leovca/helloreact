67271e9997feb0497f0d22936f8a890e9a045982
Leonan Teixeira <leonan.teixeira@gmail.com>
2016-10-05 23:50:23 -0300

    First component

---
+++ component/tutorial2.js
@@ -0,0 +1,19 @@
+var CommentList = React.createClass({
+  render: function() {
+      return (
+        <div className='commentList'>
+          Hello, Word! I am a CommentList.
+        </div>
+      );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, Word! I am a CommentForm.
+      </div>
+    );
+  }
+})
